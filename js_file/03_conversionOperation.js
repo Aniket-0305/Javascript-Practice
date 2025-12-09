aa0e5b3325e80c090bde80e5358aa9172526ddd8
Aniket@0305 <157488696+Aniket-0305@users.noreply.github.com>
2025-12-09 04:07:05 +0000

    conversion of datatypes

---
+++ js_file/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = "aniket"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+
+//"33"=> 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// let isLoggedId = "aniket"
+
+//let booleanIsLoggedId = Boolean(isLoggedId)
+//console.log(booleanIsLoggedId);
+
+//1 => true ; 0 => false
+// " " => false
+// "Aniket" => true 
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
